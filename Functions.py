5bf07bbd3fd137e0adfee51a33a475b9c62a1e6b
pivarcsi <pivarcsiadam@gmail.com>
2019-10-29 18:28:56 +0100

    ai working

--- Functions.py
+++ Functions.py
@@ -1,44 +1,178 @@
-def start_board():
-    matrix=[]
-    size=1
-    while size<3 or size>10:
-        size=int(input("The size of the board: "))
-    for i in range(size):
-        row=[]
+import copy
+from termcolor import *
+
+def init_board(matrix_size):
+    matrix = []
+    for i in range(matrix_size):
+        row = []
         matrix.append(row)
-        for y in range(size):
+        for y in range(matrix_size):
             row.append(0)
     return matrix
 
-
-
-def player_move(matrix):
-    valid_coordinate=False
-    max_lenght=len(matrix)
-    while valid_coordinate==False:
-        row=int(input("Give a row: "))-1
-        col=int(input("Give a column: "))-1
-        if (row<=max_lenght and row>0 and col<=max_lenght and col>0):
-            if matrix[row][col]==0:
-                valid_coordinate=True
+def get_move(old_board, matrix_size):
+    board = old_board
+    raw_row_list = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
+    raw_col_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    row_list = raw_row_list[0:matrix_size]
+    col_list = raw_col_list[0:matrix_size]
+    row = None
+    col = None
+    c = 0
+    while c == 0:
+        while not row in row_list:
+            try:
+                row = input("Give me the row's letter: ").upper()
+            except:
+                print("Please enter a valid letter!")
+                continue
+        while not col in col_list:
+            try:
+                col = int(input("Give me the column's number: "))
+            except:
+                print("Please enter a valid number!")
+                continue
+        if board[row_list.index(row)][col_list.index(col)] != 0:
+            row = None
+            col = None
+            print("Someone already put there!")
+            continue
         else:
-            print("Wrong input, try again!")
-    return row,col
-    
-def move(matrix,coord,player):
-    if player==1:
-        matrix[coord[0]][coord[1]]=1
-    if player==2:
-        matrix[coord[0]][coord[1]]=2
-    return matrix
+            c = 1
+    coord = (row_list.index(row), col_list.index(col))
+    return coord
+
+def mark(move, main_board, player):
+    board = main_board
+    row = move[0]
+    col = move[1]
+    board[row][col] = player
+    return board
+
+def print_board(x, matrix_size):
+    board = copy.deepcopy(x)
+    for i in range(len(x)):
+        for y in range(len(x[i])):
+            if x[i][y] == 0:
+                board[i][y] = '.'
+            elif x[i][y] == 1:
+                board[i][y] = colored('X', 'red', attrs=['bold'])
+            elif x[i][y] == 2:
+                board[i][y] = colored('O', 'green', attrs=['bold'])
+    raw_col = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
+    colum = raw_col[0:matrix_size]
+    colum = '   '.join(colum)
+    raw_row = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J']
+    row = raw_row[0:matrix_size]
+    print('    ', colum)
+    count = 0
+    while count < matrix_size:
+        row_board = board[count]
+        row_board = ' | '.join(row_board)
+        print(row[count], '  ', row_board)
+        count += 1
+
+def negative_check(raw_board_line):
+    for i in range(len(raw_board_line)):
+        for y in range(len(raw_board_line[i])):
+            if raw_board_line[i][y][0] < 0 or raw_board_line[i][y][1] < 0:
+                raw_board_line[i][y] = []
+    return raw_board_line
+
+def board_line_add(raw_board_line, board):
+    new_board_line = []
+    for i in range(len(raw_board_line)):
+        for y in range(len(raw_board_line[i])):
+            if [] in raw_board_line[i]:
+                raw_board_line[i][y] = []
+    for w in range(len(raw_board_line)):
+        new_board_line.append([])
+        for z in range(len(raw_board_line[w])):
+            if raw_board_line[w][z] != []:
+                try:
+                    new_board_line[w].append(board[raw_board_line[w][z][0]][raw_board_line[w][z][1]])
+                except:
+                    continue
+            else:
+                continue
+    return new_board_line
+
+def win_check_list(board, matrix_size, row, col):
+    index_change = [[[0, 0], [0, 1], [0, 2], [0, 3], [0, 4]],
+                    [[0, 0], [-1, 1], [-2, 2], [-3, 3], [-4, 4]],
+                    [[0, 0], [1, 1], [2, 2], [3, 3], [4, 4]],
+                    [[0, 0], [1, 0], [2, 0], [3, 0], [4, 0]]]
+    raw_board_line = [[], [], [], []]
+    if matrix_size < 5:
+        for i in range(4):
+            for y in range(3):
+                raw_board_line[i].append([row + index_change[i][y][0], col + index_change[i][y][1]])
+    elif matrix_size > 4 and matrix_size < 8:
+        for i in range(4):
+            for y in range(4):
+                raw_board_line[i].append([row + index_change[i][y][0], col + index_change[i][y][1]])
+    elif matrix_size > 7:
+        for i in range(4):
+            for y in range(5):
+                raw_board_line[i].append([row + index_change[i][y][0], col + index_change[i][y][1]])
+    board_line = board_line_add(negative_check(raw_board_line), board)
+    return board_line
+
+def has_won(board, matrix_size):
+    if matrix_size < 5:
+        player1_win = [1, 1, 1]
+        player2_win = [2, 2, 2]
+        for i in range(len(board)):
+            for y in range(len(board[i])):
+                check = win_check_list(board, matrix_size, i, y)
+                if player1_win in check:
+                    return True, 1
+                elif player2_win in check:
+                    return True, 2
+                else:
+                    continue
+        return False, 0
+
+    elif matrix_size > 4 and matrix_size < 8:
+        player1_win = [1, 1, 1, 1]
+        player2_win = [2, 2, 2, 2]
+        for i in range(len(board)):
+            for y in range(len(board[i])):
+                if player1_win in win_check_list(board, matrix_size, i, y):
+                    return True, 1
+                elif player2_win in win_check_list(board, matrix_size, i, y):
+                    return True, 2
+                else:
+                    continue
+        return False, 0
+
+    elif matrix_size > 7:
+        player1_win = [1, 1, 1, 1, 1]
+        player2_win = [2, 2, 2, 2, 2]
+        for i in range(len(board)):
+            for y in range(len(board[i])):
+                if player1_win in win_check_list(board, matrix_size, i, y):
+                    return True, 1
+                elif player2_win in win_check_list(board, matrix_size, i, y):
+                    return True, 2
+                else:
+                    continue
+        return False, 0
 
 
-if __name__ == '__main__':
-    board = start_board()
-    coord=player_move(board)
-    player=1
-    move(board,coord,player)
-    
+def is_full(board):
+    fullboard = board
+    board_size = len(board)
+    check = 0
+    for i in fullboard:
+        if 0 not in i:
+            check += 1
+        else:
+            continue
+    if check == board_size:
+        return True
+    else:
+        return False
         
         
 
--- Main.py
+++ Main.py
@@ -1,79 +1,176 @@
-import Functions
-import os
+from Functions import *
 from art import *
 from termcolor import colored, cprint
+import os
+from ai import *
+import time
+
 
 def main_menu():
-    text=text2art('In', font='broadway',chr_ignore=True)
-    text2=text2art('Row', font='broadway',chr_ignore=True)
-    cprint(text, "yellow", attrs=["bold"], )
+    text=text2art('Tic', font='broadway',chr_ignore=True)
+    text2=text2art('Tac', font='broadway',chr_ignore=True)
+    text3=text2art('Tou', font='broadway',chr_ignore=True)
+    cprint(text, "red", attrs=["bold"], )
     cprint(text2, "white", attrs=["bold"])
-    cprint("by In 3 Calories\n", "cyan", attrs=["bold"])
+    cprint(text3, "green", attrs=["bold"])
+    cprint("by In 2 Calories\n", "cyan", attrs=["bold"])
     gameplay = 0
+    print('MENU')
     print(' (1) Player vs Player\n', '(2) Player vs AI\n', '(3) Exit')
-    menunumber = int(input())
-    return menunumber
-
-def pvp():
-    Functions.startboard()
-    while Functions.resultprint!=1:
-        Functions.player_move()
-        Functions.move()
-        Functions.printboard()
-        Functions.wincheck()
-        Functions.fullcheck()
-        Functions.resultprint()
-
+    a = int(input("Give me the menu's number: "))
+    if a == 1:
+        gameplay = 1
+    elif a == 2:
+        gameplay = 2
+    elif a == 3:
+        gameplay = 3
+    return gameplay
 
 def PvP():
-    name1 = input("Player1's name? ")
-    name2 = input("Player2's name? ")
+    Nam1 = input("Player1's name? ")
+    Nam2 = input("Player2's name? ")
     play = 1
-    player=1
-    while play!=0:
-        board=Functions.startboard()
-        game=1
-        while game==1:
-            if player==1:
-                print(name1+" turn!")
+    player = 1
+    win1 = text2art(Nam1 + ' is the winner')
+    win2 = text2art(Nam2 + 'is the winner')
+    play_draw = text2art('Draw')
+    matrix_size = int(input("Give me the board's size (3-10): "))
+    while play == 1:
+        game = 1
+        board = init_board(matrix_size)
+        while game == 1:
+            os.system('clear')
+            print_board(board, matrix_size)
+            if player == 1:
+                print('The next is ' + Nam1)
             else:
-                print(name2+" turn!")
-            coord=Functions.player_move(board)
-            board=Functions.move(coord,board,player)
-            if player==1:
-                player=2
+                print('The next is ' + Nam2)
+            coord = get_move(board, matrix_size)
+            mark(coord, board, player)
+            if player == 1:
+                player = 2
             else:
-                player=1
-            Functions.printboard(board)
-            actual_result=Functions.wincheck(board)
-            if actual_result==1:
-                print(name1+ ' has won!')
-                game=0
-            if actual_result==2:
-                print(name2+" has won!")
-                game=0
-            gamedraw=Functions.fullcheck(board)
-            if gamedraw==1:
-                print("This game is a Draw!")
-                game=0
-            replay=input("Do you want to play again?: ")
-            if replay=="n":
-                play=0
-    return None
-    
-           
+                player = 1
+            win = has_won(board, matrix_size)
+            if win[0]:
+                if win[1] == 1:
+                    os.system('clear')
+                    print_board(board, matrix_size)
+                    cprint(win1, color='red', attrs=['bold'])
+                    replay = input('Are you replay? y or n: ')
+                    if replay == 'y':
+                        player = 2
+                        game = 0
+                        continue
+                    elif replay == 'n':
+                        play = 0
+                    game = 0
+                elif win[1] == 2:
+                    os.system('clear')
+                    print_board(board, matrix_size)
+                    cprint(win2, color='green', attrs=['bold'])
+                    replay = input('Are you replay? y or n: ')
+                    if replay == 'y':
+                        player = 1
+                        game = 0
+                        continue
+                    elif replay == 'n':
+                        play = 0
+                    game = 0
+            draw = is_full(board)
+            if game == 1:
+                if draw:
+                    os.system('clear')
+                    print_board(board, matrix_size)
+                    print(play_draw)
+                    replay = input('Are you replay? y or n: ')
+                    if replay == 'y':
+                        game = 0
+                        continue
+                    elif replay == 'n':
+                        play = 0
+                    game = 0
 
-os.system('clear')
-prog = 1
-while prog == 1:
+
+def PvA():
+    Nam1 = input("Player1's name? ")
+    play = 1
+    win1 = text2art(Nam1 + ' is the winner')
+    win2 = text2art('Computer is the winner')
+    play_draw = text2art('Draw')
+    matrix_size = int(input("Give me the board's size (3-10): "))
+    player = 1
+    while play == 1:
+        game = 1
+        board = init_board(matrix_size)
+        while game == 1:
+            os.system('clear')
+            print_board(board, matrix_size)
+            if player == 1:
+                print('The next is ' + Nam1)
+                coord = get_move(board, matrix_size)
+            else:
+                print('The next is the Computer')
+                time.sleep(1)
+                coord = ai_move(board)
+                print(coord)
+            mark(coord, board, player)
+            if player == 1:
+                player = 2
+            else:
+                player = 1
+            win = has_won(board, matrix_size)
+            if win[0]:
+                if win[1] == 1:
+                    os.system('clear')
+                    print_board(board, matrix_size)
+                    cprint(win1, color='red', attrs=['bold'])
+                    replay = input('Are you replay? y or n: ')
+                    if replay == 'y':
+                        game = 0
+                        continue
+                    elif replay == 'n':
+                        play = 0
+                    game = 0
+                elif win[1] == 2:
+                    os.system('clear')
+                    print_board(board, matrix_size)
+                    cprint(win2, color='green', attrs=['bold'])
+                    replay = input('Are you replay? y or n: ')
+                    if replay == 'y':
+                        game = 0
+                        continue
+                    elif replay == 'n':
+                        play = 0
+                    game = 0
+            draw = is_full(board)
+            if game == 1:
+                if draw:
+                    os.system('clear')
+                    print_board(board, matrix_size)
+                    print(play_draw)
+                    replay = input('Are you replay? y or n: ')
+                    if replay == 'y':
+                        game = 0
+                        continue
+                    elif replay == 'n':
+                        play = 0
+                    game = 0
+
+
+
+if __name__ == '__main__':
     os.system('clear')
-    game = main_menu()
-    if game == 1:
-        pvp()
-    elif game == 2:
-        pvA()
-    elif game == 3:
-        prog = 0
-os.system('clear')
-byetext=text2art("Good Bye", font='broadway', chr_ignore=True)
-cprint(byetext, 'yellow', attrs=['bold'])
+    prog = 1
+    while prog == 1:
+        os.system('clear')
+        game = main_menu()
+        if game == 1:
+            PvP()
+        elif game == 2:
+            PvA()
+        elif game == 3:
+            prog = 0
+    os.system('clear')
+    byetext=text2art("Good bye\n see you soon!", font='broadway', chr_ignore=True)
+    cprint(byetext, 'yellow', attrs=['bold'])
